e0a2d0f8a345b9aed9d38814b9280a0fb43f3d7a
Gunnaway <stevengunneweg@gmail.com>
2017-01-20 23:37:33 +0100

    victims now boost each other on collision

--- Assets/_Scripts/Victim.cs
+++ Assets/_Scripts/Victim.cs
@@ -7,6 +7,7 @@ public class Victim : MonoBehaviour {
 
 	public Action OnVictimKill;
     public Action OnVictimFinished;
+	private float CollisionMagnitude;
 
     private SphereCollider sphereCollider;
 
@@ -21,14 +22,23 @@ public class Victim : MonoBehaviour {
 	}
 
     private void OnCollisionEnter(Collision collision) {
+		if (collision.collider.gameObject.layer == LayerMask.NameToLayer ("Victims")) {
+			this.CollisionMagnitude = GetComponent<Rigidbody>().velocity.magnitude;
+		}
+
         if(collision.gameObject.GetComponent<Deadzone>() != null){
             Kill();
         }else if(collision.gameObject.GetComponent<Finish>() != null){
             Finish();
         }
-
-
     }
+
+	private void OnCollisionExit(Collision collision) {
+		if (this.CollisionMagnitude != 0) {
+			GetComponent<Rigidbody> ().velocity = GetComponent<Rigidbody> ().velocity.normalized * (this.CollisionMagnitude * 3);
+			this.CollisionMagnitude = 0;
+		}
+	}
 
     public void Finish(){
         if(OnVictimFinished != null){
