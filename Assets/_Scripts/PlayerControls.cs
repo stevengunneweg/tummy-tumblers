21c0d2e50bd43f0809be28927936cc4e684d15d5
Goosfrabix <goosfraba05@hotmail.com>
2017-01-20 23:45:44 +0100

    added xaxis for P1 P2 P3 P4 to input manager and added controlls to player controls for P1

--- Assets/_Scripts/PlayerControls.cs
+++ Assets/_Scripts/PlayerControls.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour {
 
+    private int _playerIndex = 0;
+    private Rigidbody _rigidBody;
+    public float speed =15;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        if (GetComponent<Victim>()!=null)
+        {
+            //_playerIndex = this.GetComponent<Victim>().playerIndex;
+        }
+        if (GetComponent<Rigidbody>() != null)
+        {
+            _rigidBody = GetComponent<Rigidbody>();
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (_rigidBody != null)
+        {
+            if (Input.GetAxis("P1_Xaxis") > 0.05)
+                _rigidBody.AddForce( Vector3.right* speed);
+            if (Input.GetAxis("P1_Xaxis") < -0.05)
+                _rigidBody.AddForce(-Vector3.right* speed);
+        }
+    }
 }
