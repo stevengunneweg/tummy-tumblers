4f174a0ac17cc81a3bb4ef9f49de622f2af78ca9
Gunnaway <stevengunneweg@gmail.com>
2017-01-20 21:18:52 +0100

    added ray cast to mouse to spawn hill?

--- Assets/_Scripts/MountainCaster.cs
+++ Assets/_Scripts/MountainCaster.cs
@@ -9,12 +9,19 @@ public class MountainCaster : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0)) {
+      RaycastHit hit;
+      Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+      if (Physics.Raycast(ray, out hit)) {
+        SpawnObject(hit.point);
+        // Do something with the object that was hit by the raycast.
+      }
+    }
 	}
     void SpawnObject()
     {
