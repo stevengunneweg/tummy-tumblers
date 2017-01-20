8d453436ef3a20dfe5e2dc7d2b5b76c2085c03ad
Gunnaway <stevengunneweg@gmail.com>
2017-01-21 00:14:27 +0100

    edit map instead of spawning hill

--- Assets/_Scripts/MountainCaster.cs
+++ Assets/_Scripts/MountainCaster.cs
@@ -7,8 +7,14 @@ public class MountainCaster : MonoBehaviour {
     [SerializeField]
     private GameObject _hillPrefab;
 
+	[SerializeField]
+	private AnimationCurve _increaseCurve;
+
+	private Mountain _mountain;
+
 	// Use this for initialization
 	void Start () {
+		this._mountain = FindObjectOfType<Mountain> ();
 	}
 
 	// Update is called once per frame
@@ -18,7 +24,9 @@ public class MountainCaster : MonoBehaviour {
       		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
       		if (Physics.Raycast(ray, out hit, 9999999999, LayerMask.GetMask("Mountain"))) {
-        		SpawnObject(hit.point);
+				this._mountain.Increase (hit.point, 3, this._increaseCurve, 0.5f);
+
+        		//SpawnObject(hit.point);
       		}
     	}
     }
