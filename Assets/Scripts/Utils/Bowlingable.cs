eb26e31fb62d72e9e58ca615f7fd4c16259a1d69
simonkarman <mail@simonkarman.nl>
2017-01-21 18:12:18 +0100

    Added Fence Graphics and better collision system

---
+++ Assets/Scripts/Utils/Bowlingable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bowlingable : MonoBehaviour {
+
+    protected void OnCollisionEnter(Collision collision) {
+        if (collision.transform.GetComponent<Victim>() != null) {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+            rb.AddForce(collision.relativeVelocity, ForceMode.Impulse);
+            gameObject.layer = LayerMask.NameToLayer("NonCollidingProp");
+        }
+    }
+}
