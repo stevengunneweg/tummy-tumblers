e1b52955553f65eb0c8e1d15e861126a5c041460
simonkarman <mail@simonkarman.nl>
2017-01-21 04:19:59 +0100

    Fixed Lerp To parent bug and added temp Victim graphic

--- Assets/Scripts/Utils/LerpToParent.cs
+++ Assets/Scripts/Utils/LerpToParent.cs
@@ -7,8 +7,12 @@ public class LerpToParent : MonoBehaviour {
     private Vector3 lastPosition;
     public float speed = 1f;
     
+    protected void Awake() {
+        lastPosition = transform.parent.position;
+    }
+
 	protected void LateUpdate() {
-        transform.position = Vector3.MoveTowards(lastPosition, transform.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(lastPosition, transform.parent.position, speed * Time.deltaTime);
         lastPosition = transform.position;
     }
 }
