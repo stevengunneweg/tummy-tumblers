5e3b7b391ca01b561dc3c14c1211f95de418c86d
Goosfrabix <goosfraba05@hotmail.com>
2017-01-22 13:15:04 +0100

    addedsplashscreen

---
+++ Assets/SplashScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour {
+    float timer = 2;
+	// Use this for initialization
+	void Start () {
+        timer = 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (timer <= 0)
+        {
+            if (transform.position.y > -1000)
+                transform.position += -Vector3.up * 15;
+        }
+        else
+            timer -= Time.deltaTime;
+
+    }
+}
