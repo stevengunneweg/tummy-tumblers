57d0483c49964750aa4c35142588eb3b2764baa2
Gunnaway <stevengunneweg@gmail.com>
2017-01-22 09:56:51 +0100

    added tuut for when victim finishes

--- Assets/Scripts/Victim.cs
+++ Assets/Scripts/Victim.cs
@@ -10,7 +10,8 @@ public class Victim : MonoBehaviour {
 	public List<AudioClip> soundsFemale;
 	public AudioClip soundScreamMale;	
 	public AudioClip soundScreamFemale;
-    public AudioSource explosionSound;
+	public AudioSource finishSound;
+	public AudioSource explosionSound;
     
     [HideInInspector]
     public Player player;
@@ -110,8 +111,10 @@ public class Victim : MonoBehaviour {
 
     public void Finish(){
         player.score++;
-        Effects.instance.Do(Effects.EffectType.FireWorks, transform.position);
-        Destroy(gameObject);
+		Effects.instance.Do(Effects.EffectType.FireWorks, transform.position);
+		finishSound.Play();
+		gfx.gameObject.SetActive(false);
+		Destroy(gameObject, 2);
     }
 
     public void Kill(){
